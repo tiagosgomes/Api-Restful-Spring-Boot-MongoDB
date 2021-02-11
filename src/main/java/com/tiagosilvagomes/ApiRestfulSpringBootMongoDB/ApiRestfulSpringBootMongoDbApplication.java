01e02ff5c6d7c9172a70858a0d9d67e64e605029
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-11 13:17:19 -0300

    Projeto Api-Restful-Spring-Boot-MongoDB Criado!

---
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/ApiRestfulSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringBootMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/ApiRestfulSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
