bf2099db4de3e1580d82a9c51904ca3b6383d6f7
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-11 18:00:31 -0300

    Inserindo Usuário Com Método POST!

--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/resources/UserResource.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain.User;
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.dto.UserDTO;
@@ -33,5 +37,13 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = userService.fromDTO(objDto);
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/UserService.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain.User;
+import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.dto.UserDTO;
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.repositories.UserRepository;
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
