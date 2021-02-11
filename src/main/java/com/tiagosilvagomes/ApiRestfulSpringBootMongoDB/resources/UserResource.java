63d95cb3767686d04d63aa39a9afd13d9406b81c
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-11 16:53:00 -0300

    Padrão DTO Para Retornar Usuários Implementado!

--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/resources/UserResource.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain.User;
+import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.dto.UserDTO;
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.services.UserService;
 
 @RestController
@@ -20,8 +22,9 @@ public class UserResource {
 
 	// Também pode ser @GetMapping, dá na maesma.
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
