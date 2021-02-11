e5ee3afeb61eac70a8dc3f9f22d2504cb1bce7e4
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-11 17:24:17 -0300

    Find User By Id Implementado!

--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/UserService.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/UserService.java
@@ -1,12 +1,14 @@
 package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain.User;
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.repositories.UserRepository;
+import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 }
---
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
