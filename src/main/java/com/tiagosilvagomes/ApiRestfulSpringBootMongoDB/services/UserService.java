05fa17dba9f2610c9477b92cabc63b54a6bad89b
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-11 18:08:56 -0300

    Delete User Implementado!

--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/UserService.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/UserService.java
@@ -29,6 +29,11 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
 
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
