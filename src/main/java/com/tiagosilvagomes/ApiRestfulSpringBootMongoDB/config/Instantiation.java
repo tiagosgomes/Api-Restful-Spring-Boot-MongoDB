b5214398ed4cb1463c7ac6a2d21bf2aacc36384c
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-11 16:39:09 -0300

    Instanciação da Base de Dados do MongoDB!

---
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain.User;
+import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
