f0034b05d4d48eaa99189477309e690eacb6e2e9
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-12 11:25:33 -0300

    Projetando Dados do Author com DTO Implementado!

--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/config/Instantiation.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain.Post;
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain.User;
+import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.dto.AuthorDTO;
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.repositories.PostRepository;
 import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.repositories.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
---
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/dto/AuthorDTO.java
@@ -0,0 +1,38 @@
+package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.dto;
+
+import java.io.Serializable;
+
+import com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain.User;
+
+public class AuthorDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+
+	public AuthorDTO() {
+
+	}
+
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
