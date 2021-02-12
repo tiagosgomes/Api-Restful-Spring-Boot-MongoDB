7d040758319573974519acad64f72f0c775cd2e7
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-12 10:07:54 -0300

    Update User Com Método PUT Implementado!

--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/resources/UserResource.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/resources/UserResource.java
@@ -52,4 +52,12 @@ public class UserResource {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = userService.fromDTO(objDto);
+		obj.setId(id);
+		obj = userService.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/UserService.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/services/UserService.java
@@ -29,10 +29,21 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 
 	public User fromDTO(UserDTO objDto) {
