6a206cde3079d49de6b08b386f4d2ddf22ab3293
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-12 12:18:06 -0300

    Adicionando Referência aos Posts do Usuário!

--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/config/Instantiation.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/config/Instantiation.java
@@ -43,5 +43,8 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
--- src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/domain/User.java
+++ src/main/java/com/tiagosilvagomes/ApiRestfulSpringBootMongoDB/domain/User.java
@@ -1,8 +1,11 @@
 package com.tiagosilvagomes.ApiRestfulSpringBootMongoDB.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 	}
@@ -47,6 +53,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
